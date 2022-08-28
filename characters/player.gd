7a55ee7fe154d10cb7aee8e31e5d55dcae41a74b
Jakob Steinn <jstoone@drk.sh>
2022-08-28 21:01:54 +0200

    ✨ Add knockback, when hit by enemy.

--- characters/player.gd
+++ characters/player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
+const FRICTION := 500
+
 @export var move_speed : float = 5000;
 @export var starting_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
+@onready var Hitbox := $Hurtbox
+
+var knockback := Vector2.ZERO
+
 
 ## TODO
 # - Utilize acceleration and friction -> https://www.youtube.com/watch?v=EQA9MJ5_TxU&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=2
@@ -16,7 +22,12 @@ func _physics_process(delta) -> void:
     # get input direction
     var input_direction : Vector2 = Input.get_vector("left", "right", "up", "down") * move_speed
     
-    velocity = input_direction * delta
+       
+    if knockback != Vector2.ZERO:
+        knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+        velocity = knockback
+    else:
+        velocity = input_direction * delta
     
     update_animation_parameters(velocity)
     
@@ -36,3 +47,9 @@ func pick_new_state():
         state_machine.travel("Walk")
     else:
         state_machine.travel("Idle")
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+    knockback = body.position.direction_to(position) * 200
+    if body is Zombie:
+        body.kill()

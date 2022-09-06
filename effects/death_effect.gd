ee1d10ea9f25c58ca35a28ba1ea13d1e151fbe5f
Jakob Steinn <jstoone@drk.sh>
2022-09-06 22:31:56 +0200

    😅 Death effect done... for now.
    ... it just needs the color part.

--- effects/death_effect.gd
+++ effects/death_effect.gd
@@ -1,16 +1,14 @@
 extends GPUParticles2D
 
-@export var death_impulse := 100
-var knockback := Vector2.ZERO
-
+@export var death_impulse := 500
 @onready var player := $"../../player" as Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    emitting = true
-    
-    knockback = player.position.direction_to(position) * death_impulse
+    var knockback := player.position.direction_to(position) * death_impulse
     var knockback3D := Vector3(knockback.x, knockback.y, 0);
     
     process_material.set('direction', knockback3D)
+    
+    emitting = true
     
